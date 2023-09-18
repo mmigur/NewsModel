a09170dd2b29f02fe13b2c3823bcb8a42275d080
grimma2 <80467627+grimma2@users.noreply.github.com>
2023-09-18 19:56:11 +0500

    add working good model

--- config2.py
+++ config2.py
@@ -2,14 +2,50 @@ import pandas as pd
 from transformers import pipeline
 
 
-classifier = pipeline("zero-shot-classification")
+classifier = pipeline(
+    task="zero-shot-classification",
+	model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli"
+)
 
-posts = pd.read_excel('./Model/Notebooks/Data/ai_bot_app_post.xls')
-candidate_labels = "Финансы, Технологии, Политика, Шоубиз, Fashion, Крипта, Путешествия/релокация, Образовательный контент, Развлечения, Общее".split(', ')
+posts = pd.read_excel('./Model/Notebooks/Data/posts (1).xlsx')
+candidate_labels = [
+    "Блоги",
+    "Новости и СМИ",
+    "Развлечения и юмор",
+    "Технологии",
+    "Экономика",
+    "Бизнес и стартапы",
+    "Криптовалюты",
+    "Путешествия",
+    "Маркетинг, PR, реклама",
+    "Психология",
+    "Дизайн",
+    "Политика",
+    "Искусство",
+    "Право",
+    "Образование и познавательное",
+    "Спорт",
+    "Мода и красота",
+    "Здоровье и медицина",
+    "Картинки и фото",
+    "Софт и приложения",
+    "Видео и фильмы",
+    "Музыка",
+    "Игры",
+    "Еда и кулинария",
+    "Цитаты",
+    "Рукоделие",
+    "Финансы",
+    "Шоубиз",
+    "Другое",
+]
 
 
-output = classifier(list(posts['title'][:5]), candidate_labels)
+output = classifier(
+    list(posts['title'][24:25]), candidate_labels=candidate_labels
+)
 print(output)
+
 
 '''
 1) как работать с русским языком
